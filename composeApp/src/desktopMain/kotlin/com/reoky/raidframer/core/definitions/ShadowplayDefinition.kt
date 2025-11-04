744be824c793b52258c944e192aacba14a4059bb
catree <161555754+catreo@users.noreply.github.com>
2025-11-04 00:52:05 -0600

    Update ShadowplayDefinition.kt

--- composeApp/src/desktopMain/kotlin/com/reoky/raidframer/core/definitions/ShadowplayDefinition.kt
+++ composeApp/src/desktopMain/kotlin/com/reoky/raidframer/core/definitions/ShadowplayDefinition.kt
@@ -4,17 +4,17 @@ object ShadowplayDefinition : SkillTreeDefinition {
 
   override val tree = SkillTreeType.SHADOWPLAY
   override val skills = listOf(
-    Skill(0, "", 0.0, 0.0, listOf("")),
-    Skill(1, "", 0.0, 0.0, listOf("")),
-    Skill(2, " ", 0.0, 0.0, listOf("")),
-    Skill(3, " ", 0.0, 0.0, listOf("")),
-    Skill(4, " ", 0.0, 0.0, listOf("")),
-    Skill(5, "", 0.0, 0.0, listOf("")),
-    Skill(6, "", 0.0, 0.0, listOf("")),
-    Skill(7, "", 0.9, 0.0, listOf("")),
-    Skill(8, "", 0.0, 0.0, listOf("")),
-    Skill(9, " ", 0.0, 0.0, listOf("")),
-    Skill(10, "", 0.0, 0.0, listOf("")),
-    Skill(11, "", 0.0, 0.0, listOf(""))
+    Skill(0, "Rapid Strike", 0.0, 0.0, listOf("Rapid Strike")),
+    Skill(1, "Poisoned Weapons", 0.0, 7.2, listOf("Poisoned Weapons")),
+    Skill(2, "Pin Down", 0.0, 16.8, listOf("Pin Down")),
+    Skill(3, "Drop Back", 0.0, 9.6, listOf("Drop Back")),
+    Skill(4, "Overwhelm", 0.0, 14.4, listOf("Overwhelm")), // no casting
+    Skill(5, "Stalker's Mark", 0.0, 21.6, listOf("Stalker's Mark")),
+    Skill(6, "Wallop", 0.0, 9.6, listOf("Wallop")), // no casting
+    Skill(7, "Stealth", 0.9, 25.0, listOf("Stealth")),
+    Skill(8, "Freerunner", 0.0, 71.0, listOf("Freerunner")),
+    Skill(9, "Shadowsmite", 0.0, 19.2, listOf("Shadowsmite")),
+    Skill(10, "Leech", 0.0, 36.0, listOf("Leech")),
+    Skill(11, "Throw Dagger", 0.0, 28.0, listOf("Throw Dagger")) // no casting
   )
 }
