392cfca005288eb6c482d656ca4a69e49d37c0e3
catree <161555754+catreo@users.noreply.github.com>
2025-11-07 03:21:19 -0600

    Gunslinger and Songcraft Definitions

--- composeApp/src/desktopMain/kotlin/com/reoky/raidframer/core/definitions/GunslingerDefinition.kt
+++ composeApp/src/desktopMain/kotlin/com/reoky/raidframer/core/definitions/GunslingerDefinition.kt
@@ -4,17 +4,17 @@ object GunslingerDefinition : SkillTreeDefinition {
 
   override val tree = SkillTreeType.GUNSLINGER
   override val skills = listOf(
-    Skill(0, "", 0.0, 0.0, listOf("")),
-    Skill(1, "", 0.0, 0.0, listOf("")),
-    Skill(2, " ", 0.0, 0.0, listOf("")),
-    Skill(3, " ", 0.0, 0.0, listOf("")),
-    Skill(4, " ", 0.0, 0.0, listOf("")),
-    Skill(5, "", 0.0, 0.0, listOf("")),
-    Skill(6, "", 0.0, 0.0, listOf("")),
-    Skill(7, "", 0.0, 0.0, listOf("")),
-    Skill(8, "", 0.0, 0.0, listOf("")),
-    Skill(9, " ", 0.0, 0.0, listOf("")),
-    Skill(10, "", 0.0, 0.0, listOf("")),
-    Skill(11, "", 0.0, 0.0, listOf(""))
+    Skill(0, "Ceaseless Fire", 0.0, 0.0, listOf("Ceaseless Fire")), // no casting
+    Skill(1, "Corrosive Barrage", 0.0, 8.0, listOf("Corrosive Barrage")), //  no casting
+    Skill(2, "Backdraft", 0.4, 16.0, listOf("Backdraft")), // no casting Backdraft
+    Skill(3, "Splinter Shell", 0.0, 40.0, listOf("Splinter Shell")), // no casting
+    Skill(4, "Blight Bolt", 0.0, 23.0, listOf("Blight Bolt")), // no casting
+    Skill(5, "Room Sweeper", 0.0, 5.0, listOf("Room Sweeper")), // Room Sweeper buff only, no casting
+    Skill(6, "Vicious Rebuke", 0.0, 16.0, listOf("Vicious Rebuke")),
+    Skill(7, "Tactical Roll", 0.0, 8.0, listOf("Tactical Roll")), // no casting / Tactical Roll buff only
+    Skill(8, "Trigger Happy", 0.0, 24.0, listOf("Trigger Happy")),
+    Skill(9, "Reversal", 0.0, 45.0, listOf("Reversal")), // no casting, no dmg, only debuffs applied
+    Skill(10, "Sniper's Bane", 0.0, 40.0, listOf("Sniper's Bane")),  // no casting, no dmg, Bane debuff applied to target
+    Skill(11, "Collateral Damage", 1.8, 40.0, listOf("Collateral Damage")) // no casting
   )
 }
