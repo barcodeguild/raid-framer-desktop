8885d3630e595523e0222c176a968288b44ea4c3
reoky <161555754+catreo@users.noreply.github.com>
2025-12-03 20:55:02 -0600

    Database Dep Injection Pattern
    This will help so that we aren't passing dao objects between classes and don't have to initialize interactors. They should just be able to

---
+++ composeApp/src/desktopMain/kotlin/com/reoky/raidframer/core/database/Injection.kt
@@ -0,0 +1,21 @@
+package com.reoky.raidframer.core.database
+
+/**
+ * Dependency injection object singleton for database-related components.
+ * The idea is that once initialized in main, its members can be accessed
+ * globally without having to pass them to components like the interactors. mhmm!
+ */
+object RFDao {
+  lateinit var configDao: ConfigDao
+    private set
+  lateinit var playerCacheDao: PlayerCacheDao
+    private set
+  lateinit var windowStateDao: WindowStateDao
+    private set
+
+  fun init(db: AppDatabase) {
+    configDao = db.getConfigDao()
+    playerCacheDao = db.getPlayerCacheDao()
+    windowStateDao = db.getWindowStateDao()
+  }
+}
