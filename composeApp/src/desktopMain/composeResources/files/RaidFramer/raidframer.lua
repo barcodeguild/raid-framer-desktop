07659fea53cb3c5b8a3418023229756ba14a8e48
catree <161555754+catreo@users.noreply.github.com>
2026-07-03 17:49:11 -0500

    Version Bump 2.1.1

--- composeApp/src/desktopMain/composeResources/files/RaidFramer/raidframer.lua
+++ composeApp/src/desktopMain/composeResources/files/RaidFramer/raidframer.lua
@@ -3,7 +3,7 @@
 -- https://github.com/barcodeguild/raid-framer-desktop
 -- Author: Reoky
 RF = RF or {}
-RF.TAG = "Raid Framer 2.1.0"
+RF.TAG = "Raid Framer 2.1.1"
 
 RF.PLAYER_NAME = ""
 RF.FACTION = ""
